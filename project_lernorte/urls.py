1d993f1fdbcf36e9c8354409fff51076d331bc77
andrise4274 <andri.seiz@breitband.ch>
2024-08-21 21:24:38 +0200

    chloepfgatter updloaded with smaller pictures

--- project_lernorte/urls.py
+++ project_lernorte/urls.py
@@ -28,7 +28,5 @@ urlpatterns = [
 
 
 # debug=True media workaround
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
