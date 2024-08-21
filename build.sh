60119772a17c90e0469c8b40faea47814b910374
andrise4274 <andri.seiz@breitband.ch>
2024-08-21 20:30:49 +0200

    new try

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
-# build_files.sh
-pip install -r requirements.txt
-
 # make migrations
+python3.12 manage.py makemigrations lernorte
 python3.12 manage.py migrate 
 python3.12 manage.py collectstatic
