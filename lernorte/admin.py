c4a23eacefeaac5fa88b9faf8d2649a034934e7d
andrise4274 <andri.seiz@breitband.ch>
2024-08-22 16:10:21 +0200

    added Media table to admin.py

--- lernorte/admin.py
+++ lernorte/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, Lernort
+from .models import User, Lernort, Media
 
 
 admin.site.register(User)
 admin.site.register(Lernort)
+admin.site.register(Media)
